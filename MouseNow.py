31c1720650cc642d82098e7861bc917c440ac438
Tim Lai <timlai1208@gmail.com>
2020-06-01 14:19:42 +0800

    Update MouseNow.py

--- MouseNow.py
+++ MouseNow.py
@@ -5,8 +5,14 @@ try:
     while True:
         x, y = pyautogui.position()
         positionStr = 'X:' + str(x).rjust(4) + ' Y:' + str(y).rjust(4)
+        
+        # Add RGB of the pixel to the output
+        pixelColor = pyautogui.screenshot().getpixel((x, y))
+        colorStr = ' RGB: ' + str(pixelColor[0:3]).rjust(3)
+        positionStr += colorStr
+        
         print(positionStr, end='')
-        time.sleep(0.1)
+        time.sleep(0.5)
         print('\b' * len(positionStr), end='', flush=True)
 
 except KeyboardInterrupt:
